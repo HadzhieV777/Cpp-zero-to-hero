2c00123f05f5780b22996dcf295156faa6967eb8
Rado <bdwhadjiev42@gmail.com>
2022-01-29 10:03:45 +0200

    Simple project that finds the factorial of a number

---
+++ Factorial/Factorial/Factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Finding the factorial of a number using for loop
+
+    int number;
+    cout << "Number: ";
+    cin >> number;
+   
+    int factorial = 1;
+
+    //6!=1*2*3*4*5*6=720
+    //for (int i = 1; i <= number; i++) {
+        //factorial = factorial * i;
+    //}
+
+    //6!=6*5*4*3*2*1=720
+    for (int i = number; i >= 1; i--) {
+        factorial = factorial * i;
+    }
+    cout << number << "!=" << factorial;
+
+    system("pause>0");
+}
